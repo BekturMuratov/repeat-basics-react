996b056a68cb504b575b6bb129795392120a7ca3
BekturMuratov <muratov225@gmail.com>
2022-01-06 22:35:56 +0600

    posts components with props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
-import Counter from './components/Counter';
+import PostList from './components/PostList';
+import './styles/App.css';
 
 function App() {
-
+  let [posts, setPosts] = useState([
+    {id:1, title:"JavaScript", body:'Description'},
+    {id:2, title:"JavaScript", body:'Description'},
+    {id:3, title:"JavaScript", body:'Description'},
+    {id:4, title:"JavaScript", body:'Description'},
+  ]);
   return (
     <div className="App">
- <Counter/>
- <Counter/>
- <Counter/>
- <Counter/>
+      <form>
+        <input type="text" placeholder="Название поста" />
+        <input type="text" placeholder="Описание поста" />
+        <button>Создать пост</button>
+      </form>
+      <PostList posts={posts} title='Список постов 1' />
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function PostItem(props) {
+    return (
+        <div className="post">
+        <div className='post__content'>
+          <strong>{props.post.id}.{props.post.title}</strong>
+          <div>
+            {props.post.body}
+          </div>
+        </div>
+        <div className='post__btns'>
+          <button>Удалить</button>
+        </div>
+      </div>
+    )
+}
